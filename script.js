0ca082b633847097882b6683a2436e50045e63fc
Aman <amantechmsy@gmail.com>
2023-01-07 03:20:29 +0530

    Add preloader and wrong city call

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const place = document.querySelector("#place");
 const temperature = document.querySelector("#temperature");
 const tempRange = document.querySelector("#tempRange");
 const tempStatus = document.querySelector("#weathercon");
+// const info_box = document.querySelector("#info_box");
 
 function changeStatus(val) { 
     if(val=="Sunny") return `<i class="fas fa-sun" style="color: #eccc68;"></i>`;
@@ -14,28 +15,36 @@ function changeStatus(val) {
 }
 
 const replaceVal = (newVal) => {
+    if(newVal.cod==="404"){
+        place.innerHTML = `<i class="fas fa-street-view"></i>OOPs! City Not Found...`;
+        temperature.innerHTML = ``;
+        tempRange.innerHTML = ``;
+        tempStatus.innerHTML = ``;
+        return;
+    }
+
     let city = newVal.name;
     let country = newVal.sys.country;
     let temp = newVal.main.temp;
     let tempMin = newVal.main.temp_min;
     let tempMax = newVal.main.temp_max;
-    let modification1 = `<i class="fas fa-street-view"></i> ${city}, ${country}`
-    let modification2 = `${temp} °C`
-    let modification3 = `Min ${tempMin} °C | Max ${tempMax} °C`
-    place.innerHTML = modification1;
-    temperature.innerHTML = modification2;
-    tempRange.innerHTML = modification3;
+
+    place.innerHTML = `<i class="fas fa-street-view"></i> ${city}, ${country}`
+    temperature.innerHTML = `${temp} °C`
+    tempRange.innerHTML = `Min ${tempMin} °C | Max ${tempMax} °C`
     tempStatus.innerHTML = changeStatus(newVal.weather[0].main);
 }
 
 const getWeather = async (city) => {
     try{
+        place.innerHTML = `<i class="fas fa-spinner fa-pulse"></i>`;    //only for showing loading symbol 
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d3593db5e8552cc87239b754b39d2d0e`;
         const response = await fetch(url);
+        // if(response)
         const data = await response.json();
         return replaceVal(data);
     }catch(err){
-        console.log("City Not Found!");
+        console.log(err);
     }
 }
 
